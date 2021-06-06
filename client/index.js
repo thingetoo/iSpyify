a63f362505845dbb265d6e51039e273dd85a1815
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-05 22:41:40 -0400

    removed unnecessary imports

--- client/index.js
+++ client/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import './styles.less';
-import 'normalize.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
